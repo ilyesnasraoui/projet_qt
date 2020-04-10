2e839f56449e65aa29792337416347c4f2f41468
ilyesnasraoui <mohamedilyes.nasraoui@esprit.tn>
2020-04-10 15:09:58 +0100

    c

---
+++ khedmet ilyes nasraoui/mainwindow.cpp
@@ -0,0 +1,214 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "voiture.h"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->tableView2->setModel(tmpvoiture.afficher());
+    //ui->tableView->setModel(tmpcategorie.afficher());
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+
+
+void MainWindow::on_ajouter_clicked()
+{
+    int idv = ui->lineEdit_idv->text().toInt();
+    QString marque= ui->lineEdit_marque->text();
+    QString matricule= ui->lineEdit_matricule->text();
+    int idcategorie=ui->lineEdit_idcategorie->text().toInt();
+    int nbplaces=ui->lineEdit_nbplaces->text().toInt();
+    int anciennete=ui->lineEdit_anciennete->text().toInt();
+    QString couleur=ui->lineEdit_couleur->text();
+    int kilometrage=ui->lineEdit_kilometrage->text().toInt();
+    QString carburant=ui->lineEdit_carburant->text();
+    int prixparjour=ui->lineEdit_prixparjour->text().toInt();
+    int etat=ui->lineEdit_etat->text().toInt();
+  voiture e(idv,marque,matricule,idcategorie,nbplaces,anciennete,couleur,kilometrage,carburant,prixparjour,etat);
+  bool test=e.ajouter();
+  if(test)
+{ui->tableView2->setModel(tmpvoiture.afficher());//refresh
+QMessageBox::information(nullptr, QObject::tr("Ajouter un voiture"),
+                  QObject::tr("voiture ajouté.\n"
+                              "Click Cancel to exit."), QMessageBox::Cancel);
+
+}
+  else
+      QMessageBox::critical(nullptr, QObject::tr("Ajouter un voiture"),
+                  QObject::tr("Erreur !.\n"
+                              "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+}
+
+
+void MainWindow::on_supprimer_clicked()
+{
+int id = ui->lineEdit_ids->text().toInt();
+bool test=tmpvoiture.supprimer(id);
+if(test)
+{ui->tableView2->setModel(tmpvoiture.afficher());//refresh
+    QMessageBox::information(nullptr, QObject::tr("Supprimer une voiture"),
+                QObject::tr("voiture supprimé.\n"
+                            "Click Cancel to exit."), QMessageBox::Cancel);
+
+}
+else
+    QMessageBox::critical(nullptr, QObject::tr("Supprimer une voiture"),
+                QObject::tr("Erreur !.\n"
+                            "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+}
+
+
+void MainWindow::on_modifier_clicked()
+{
+    int idv = ui->lineEdit->text().toInt();
+    QString marque= ui->lineEdit_2->text();
+    QString matricule= ui->lineEdit_3->text();
+    int idcategorie=ui->lineEdit_4->text().toInt();
+    int nbplaces=ui->lineEdit_5->text().toInt();
+    int anciennete=ui->lineEdit_6->text().toInt();
+    QString couleur=ui->lineEdit_7->text();
+    int kilometrage=ui->lineEdit_8->text().toInt();
+    QString carburant=ui->lineEdit_9->text();
+    int prixparjour=ui->lineEdit_10->text().toInt();
+    int etat=ui->lineEdit_11->text().toInt();
+    voiture e(idv,marque,matricule,idcategorie,nbplaces,anciennete,couleur,kilometrage,carburant,prixparjour,etat);
+    bool test=e.modifier();
+  if(test)
+{ui->tableView2->setModel(tmpvoiture.afficher());//refresh
+QMessageBox::information(nullptr, QObject::tr("modifier une voiture"),
+                  QObject::tr("voiture modifier.\n"
+                              "Click Cancel to exit."), QMessageBox::Cancel);
+
+}
+  else
+      QMessageBox::critical(nullptr, QObject::tr("modifier un voiture"),
+                  QObject::tr("Erreur !.\n"
+                              "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+
+}
+
+void MainWindow::on_trie_clicked()
+{
+
+
+   ui->tableView2->setModel(tmpvoiture.trie());//refresh
+
+
+}
+
+
+// categorieee
+
+
+
+void MainWindow::on_ajouter_categorie_clicked()
+{
+    int id = ui->lineEdit_idc->text().toInt();
+    QString type= ui->lineEdit_type->text();
+    int nbvoitures =ui->lineEdit_nbvoitures->text().toInt();
+
+  categorie e(id,type,nbvoitures);
+  bool test=e.ajouter();
+  if(test)
+{ui->tableView->setModel(tmpcategorie.afficher());//refresh
+QMessageBox::information(nullptr, QObject::tr("Ajouter un categorie"),
+                  QObject::tr("categorie ajouté.\n"
+                              "Click Cancel to exit."), QMessageBox::Cancel);
+
+}
+  else
+      QMessageBox::critical(nullptr, QObject::tr("Ajouter un categorie"),
+                  QObject::tr("Erreur !.\n"
+                              "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+}
+
+
+void MainWindow::on_supprimer_categorie_clicked()
+{
+int id = ui->lineEdit_12->text().toInt();
+bool test=tmpcategorie.supprimer(id);
+if(test)
+{ui->tableView->setModel(tmpcategorie.afficher());//refresh
+    QMessageBox::information(nullptr, QObject::tr("Supprimer une categorie"),
+                QObject::tr("categorie supprimé.\n"
+                            "Click Cancel to exit."), QMessageBox::Cancel);
+
+}
+else
+    QMessageBox::critical(nullptr, QObject::tr("Supprimer une categorie"),
+                QObject::tr("Erreur !.\n"
+                            "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+}
+
+
+void MainWindow::on_modifier_categorie_clicked()
+{
+    int id = ui->lineEdit_13->text().toInt();
+    QString type= ui->lineEdit_14->text();
+    int nbvoitures=ui->lineEdit_15->text().toInt();
+    categorie e(id,type,nbvoitures);
+    bool test=e.modifier();
+  if(test)
+{ui->tableView->setModel(tmpcategorie.afficher());//refresh
+QMessageBox::information(nullptr, QObject::tr("modifier une categorie"),
+                  QObject::tr("categorie modifier.\n"
+                              "Click Cancel to exit."), QMessageBox::Cancel);
+
+}
+  else
+      QMessageBox::critical(nullptr, QObject::tr("modifier un categorie"),
+                  QObject::tr("Erreur !.\n"
+                              "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+
+}
+
+void MainWindow::on_trie_categorie_clicked()
+{
+
+
+   ui->tableView->setModel(tmpcategorie.trie());//refresh
+
+
+}
+
+
+
+void MainWindow::on_recherche_clicked()
+{
+
+   QString x = ui->lineEdit_16->text();
+   ui->tableView2->setModel(tmpvoiture.recherche(x));//refresh
+
+
+}
+
+
+
+
+
+
+
+
+
+
